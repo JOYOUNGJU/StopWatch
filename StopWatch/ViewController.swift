274e4f52d4697baeaa9d43d610b63a1439dd9ba0
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-10 15:38:56 +0900

    Start/Stop/Reset

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet var timeLable: UILabel!
+    var count = 0
+    var myTimer = Timer();
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func start(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer)
+            in self.updateTime()
+            
+        })
+        
+    }
+    func updateTime(){
+        count = count + 1
+        let min = count / 60 / 100
+        let sce = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sce * 100)
+        timeLable.text = String(format: "%02d:%02d:%02d", min, sce, msec)
+    }
+    @IBAction func stop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    
+    @IBAction func Reset(_ sender: Any) {
+        count = 0
+        let min = count / 60 / 100
+        let sce = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sce * 100)
+        timeLable.text = String(format: "%02d:%02d:%02d", min, sce, msec)
+    }
+    
 }
+        
+
+    
+    
+
+    
+
+    
+
 
